4d88aba8710db37ade719441ebe9fb61b41ca8b3
Sustic, Alessandro <alessandro.sustic@garmin.com>
2022-04-06 11:52:55 +0300

    JUnit tests for adding an assignment

--- src/test/java/AddAssignmentJUnitTest.java
+++ src/test/java/AddAssignmentJUnitTest.java
@@ -1,10 +1,7 @@
-import domain.Student;
 import domain.Tema;
 import org.junit.Test;
-import repository.StudentXMLRepo;
 import repository.TemaXMLRepo;
 import service.Service;
-import validation.StudentValidator;
 import validation.TemaValidator;
 import validation.ValidationException;
 
@@ -23,7 +20,7 @@ public class AddAssignmentJUnitTest {
     }
 
     @Test
-    public void addAssignment_Valid_Test() {
+    public void addAssignment_notInRepo_Valid_Test() {
         TemaXMLRepo temaRepo = new TemaXMLRepo("src/main/resources/Teme.xml");
         TemaValidator temaValidator = new TemaValidator();
         Service temaService = new Service(null, null, temaRepo, temaValidator, null, null);
@@ -32,5 +29,70 @@ public class AddAssignmentJUnitTest {
         assertNull(addedTema);
         assertEquals(temaService.findTema("1"), tema);
         temaService.deleteTema("1");
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addAssignment_nullId_Invalid_Test() {
+        TemaXMLRepo temaRepo = new TemaXMLRepo("src/main/resources/Teme.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        Service temaService = new Service(null, null, temaRepo, temaValidator, null, null);
+        Tema tema = new Tema(null, "WBT", 10, 5);
+        temaService.addTema(tema);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addAssignment_emptyDescription_Invalid_Test() {
+        TemaXMLRepo temaRepo = new TemaXMLRepo("src/main/resources/Teme.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        Service temaService = new Service(null, null, temaRepo, temaValidator, null, null);
+        Tema tema = new Tema("1", "", 10, 5);
+        temaService.addTema(tema);
+
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addAssignment_smallerDeadline_InValid_Test() {
+        TemaXMLRepo temaRepo = new TemaXMLRepo("src/main/resources/Teme.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        Service temaService = new Service(null, null, temaRepo, temaValidator, null, null);
+        Tema tema = new Tema("1", "SSVV", 0, 5);
+        temaService.addTema(tema);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addAssignment_biggerDeadline_InValid_Test() {
+        TemaXMLRepo temaRepo = new TemaXMLRepo("src/main/resources/Teme.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        Service temaService = new Service(null, null, temaRepo, temaValidator, null, null);
+        Tema tema = new Tema("1", "SSVV", 15, 5);
+        temaService.addTema(tema);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addAssignment_biggerReceivingDate_InValid_Test() {
+        TemaXMLRepo temaRepo = new TemaXMLRepo("src/main/resources/Teme.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        Service temaService = new Service(null, null, temaRepo, temaValidator, null, null);
+        Tema tema = new Tema("1", "SSVV", 10, 15);
+        temaService.addTema(tema);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addAssignment_smallerReceivingDate_InValid_Test() {
+        TemaXMLRepo temaRepo = new TemaXMLRepo("src/main/resources/Teme.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        Service temaService = new Service(null, null, temaRepo, temaValidator, null, null);
+        Tema tema = new Tema("1", "SSVV", 0, 15);
+        temaService.addTema(tema);
+    }
+
+    @Test
+    public void addAssignment_alreadyInRepo_Valid_Test() {
+        TemaXMLRepo temaRepo = new TemaXMLRepo("src/main/resources/Teme.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        Service temaService = new Service(null, null, temaRepo, temaValidator, null, null);
+        Tema tema = new Tema("2", "SSVV", 10, 5);
+        Tema addedTema = temaService.addTema(tema);
+        assertEquals(addedTema, tema);
     }
 }
