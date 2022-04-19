f48bad215510674930fda855ed3304a06557af19
Ale-Natalia <45286425+Ale-Natalia@users.noreply.github.com>
2022-04-19 23:56:20 +0300

    addGrade integration tests take home

--- src/test/java/AddGradeTest.java
+++ src/test/java/AddGradeTest.java
@@ -61,25 +61,49 @@ public class AddGradeTest {
 
         TemaValidator temaValidator = new TemaValidator();
         Service temaService = new Service(null, null, temaRepo, temaValidator, null, null);
-        Tema tema = new Tema("1", "WBT", 10, 5);
+        Tema tema = new Tema("01", "WBT", 10, 5);
         Tema addedTema = temaService.addTema(tema);
 
         StudentValidator studentValidator = new StudentValidator();
         Service studentService = new Service(studentRepo, studentValidator, null, null, null, null);
-        Student student = new Student("0", "Ana Popa", 917, "ana@yahoo.com");
+        Student student = new Student("00", "Ana Popa", 917, "ana@yahoo.com");
         Student foundStudent = studentService.addStudent(student);
 
         NotaXMLRepo gradeRepo = new NotaXMLRepo("src/main/resources/Note.xml");
         NotaValidator notaValidator = new NotaValidator(studentRepo, temaRepo);
         Service gradeService = new Service(studentRepo, null, temaRepo, null, gradeRepo, notaValidator);
-        Nota nota = new Nota("1", "0", "1", 10, LocalDate.now());
+        Nota nota = new Nota("01", "00", "01", 10, LocalDate.now());
         var addedNota = gradeService.addNota(nota, "well done!");
         //assertNull(addedNota);;
         //Tema tema = new Tema("2", "SSVV", 10, 5);
         assertEquals(addedNota, nota.getNota());
 
-        studentService.deleteStudent("0");
-        temaService.deleteTema("1");
-        gradeService.deleteNota("1");
+        studentService.deleteStudent("00");
+        temaService.deleteTema("01");
+        gradeService.deleteNota("01");
     }
+
+    @Test
+    public void addAssignment_Integration_Test() {
+        StudentXMLRepo studentRepo = new StudentXMLRepo("src/main/resources/Studenti.xml");
+        StudentValidator studentValidator = new StudentValidator();
+        Student student = new Student("000", "Ana Dana", 917, "ana@yahoo.com");
+
+        TemaXMLRepo temaRepo = new TemaXMLRepo("src/main/resources/Teme.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        Service temaService = new Service(studentRepo, studentValidator, temaRepo, temaValidator, null, null);
+
+        Student foundStudent = temaService.addStudent(student);
+        assertNull(foundStudent);
+        assertEquals(temaService.findStudent("000"), student);
+
+        Tema tema = new Tema("001", "Integration testing", 10, 5);
+        Tema addedTema = temaService.addTema(tema);
+        assertNull(addedTema);
+        assertEquals(temaService.findTema("001"), tema);
+
+        temaService.deleteTema("001");
+        temaService.deleteStudent("000");
+    }
+
 }
